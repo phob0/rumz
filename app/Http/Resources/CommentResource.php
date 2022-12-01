fe364d45db7586db0085ddf1e21331dc41c36c86
Alex <ph0bo@protonmail.com>
2022-12-01 20:19:24 +0200

    reply flag on comment resource

--- app/Http/Resources/CommentResource.php
+++ app/Http/Resources/CommentResource.php
@@ -17,6 +17,7 @@ class CommentResource extends JsonResource
         $response = parent::toArray($request);
 
         $response[] = [
+            "is_reply" => false,
             "liked" => $this->likes->isNotEmpty() ? $this->likes->contains(fn($item) => $item->user_id === auth()->user()->id) : false,
             "disliked" => $this->dislikes->isNotEmpty() ? $this->dislikes->contains(fn($item) => $item->user_id === auth()->user()->id) : false,
         ];
