6162cb0bf426e87c1f828ed12c569bcef157bdf1
Alex <ph0bo@protonmail.com>
2022-12-29 20:22:59 +0200

    add use of namespace JSONResource typo

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Message;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 class MessageController extends Controller
 {
