7a315644aa9b74cdf6e4d927bd00422adf1b942f
Alex <ph0bo@protonmail.com>
2023-01-19 23:17:09 +0200

    Friend flow invites

---
+++ app/Notifications/InviteFriend.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\User;
+use App\Interfaces\NotificationTypes;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class InviteFriend extends Notification implements NotificationTypes
+{
+    use Queueable;
+
+    public User $user;
+    public string $message;
+    public bool $follow_up = false;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user, $message)
+    {
+        $this->user = $user;
+        $this->message = $message;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            'user' => $this->user,
+            'message' => $this->message,
+            'follow_up' => $this->follow_up,
+            'notification_type' => self::FRIEND_INVITATION
+        ];
+    }
+}
---
+++ app/Policies/MessagePolicy.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class MessagePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function send(User $user, $channel)
+    {
+        // if first or single record or multiple but with single unique user id that equals current session => true
+        // else
+        // get both user ids
+        // compare channel with algorythm users id 
+    }
+
+    public function history(User $user, $channel)
+    {
+        $message = Message::where([
+            ['channel', '=', $channel],
+            ['user_id', '=', $user->id]
+        ]);
+
+        return $message->count() > 0 && $message->exists();
+    }
+}
